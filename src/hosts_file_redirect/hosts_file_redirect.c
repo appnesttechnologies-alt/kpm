1e779242717f53a5d14a2d9e617a0e8fc5274717
appnesttechnologies-alt <appnesttechnologies@gmail.com>
2026-07-29 23:54:13 +0530

    Update hosts_file_redirect.c

--- src/hosts_file_redirect/hosts_file_redirect.c
+++ src/hosts_file_redirect/hosts_file_redirect.c
@@ -53,6 +53,27 @@ KPM_DESCRIPTION("KPM shared-memory memory bridge via char device + mmap ring buf
 #define HFR_RING_MAGIC      0x48465231u
 #define HFR_RING_SLOTS      32
 #define HFR_IOCTL_SUBMIT 0x48465200u
+struct inode;
+struct file;
+struct vm_area_struct;
+struct poll_table_struct;
+typedef unsigned int __poll_t;
+
+
+
+struct file_operations {
+    void *owner;
+    loff_t (*llseek)(struct file *, loff_t, int);
+    ssize_t (*read)(struct file *, char __user *, size_t, loff_t *);
+    ssize_t (*write)(struct file *, const char __user *, size_t, loff_t *);
+    long (*unlocked_ioctl)(struct file *, unsigned int, unsigned long);
+    long (*compat_ioctl)(struct file *, unsigned int, unsigned long);
+    int (*mmap)(struct file *, struct vm_area_struct *);
+    unsigned int (*poll)(struct file *, struct poll_table_struct *);
+    int (*open)(struct inode *, struct file *);
+    int (*release)(struct inode *, struct file *);
+};
+
 
 
 
