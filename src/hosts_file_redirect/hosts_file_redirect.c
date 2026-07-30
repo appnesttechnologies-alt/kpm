2e72da3e69ecd9b563e1349f3252935cad92edc2
appnesttechnologies-alt <appnesttechnologies@gmail.com>
2026-07-30 15:27:45 +0530

    Update hosts_file_redirect.c

--- src/hosts_file_redirect/hosts_file_redirect.c
+++ src/hosts_file_redirect/hosts_file_redirect.c
@@ -1,3 +1,4 @@
+
 #include <compiler.h>
 #include <kpmodule.h>
 #include <linux/printk.h>
@@ -8,22 +9,21 @@
 #define ENABLE_DEBUG_LOG 1
 
 #if ENABLE_DEBUG_LOG
-    #define TAG "[FCK"
+    #define TAG "[FCK]"
     #define logv(fmt, ...) pr_info(TAG fmt, ##__VA_ARGS__)
 #else
     #define logv(fmt, ...) do {} while(0) 
 #endif
 
 
-KPM_NAME("FKR");
+KPM_NAME("FCK");
 KPM_VERSION("1.0.0");
 KPM_LICENSE("GPL v2");
-KPM_AUTHOR("FKR");
-KPM_DESCRIPTION("FKR");
+KPM_AUTHOR("FKC");
+KPM_DESCRIPTION("FCK");
 
 
 #define OP_READ_MEM                 8001
-#define OP_WRITE_MEM                8002
 #define OP_GET_CPU_NUM_BRPS         8009
 #define OP_GET_CPU_NUM_WRPS         8010
 #define OP_SET_HW_BREAKPOINT        8011
@@ -359,24 +359,23 @@ static void before_ioctl(hook_fargs4_t *args, void *udata)
                 }
             }
 
-            if (phys_addr && kf_pfn_valid(phys_addr >> my_page_shift) &&
-                kf_valid_phys_addr_range(phys_addr, chunk)) {
-                uint64_t kva = (phys_addr & (-1ULL << my_page_shift))
-                             - *(uint64_t *)kv_memstart_addr
-                             + (-1ULL << my_va_bits)
-                             + (phys_addr & ~(-1ULL << my_page_shift));
-                
-                if (cmd == OP_READ_MEM) {
-                    kf___arch_copy_to_user((void __user *)outbuf, (void *)kva, chunk);
-                } else {
-                    uint8_t tmp[128];
-                    kf___arch_copy_from_user(tmp, (void __user *)outbuf, chunk);
-                    for (uint64_t i = 0; i < chunk; i++)
-                        *(volatile uint8_t *)(kva + i) = tmp[i];
+            if (kf_mmput) kf_mmput(mm);
+
+            if (phys_addr) {
+                if (kf_pfn_valid(phys_addr >> my_page_shift) &&
+                    kf_valid_phys_addr_range(phys_addr, chunk)) {
+                    uint64_t kva = (phys_addr & (-1ULL << my_page_shift))
+                                 - *(uint64_t *)kv_memstart_addr
+                                 + (-1ULL << my_va_bits)
+                                 + (phys_addr & ~(-1ULL << my_page_shift));
+                    if (cmd == OP_READ_MEM) {
+    kf___arch_copy_to_user((void __user *)outbuf, (void *)kva, chunk);
+} else {
+    for (uint64_t i = 0; i < chunk; i++)
+        *(volatile uint8_t *)(kva + i) = ((uint8_t *)outbuf)[i];
+}
                 }
             }
-
-            if (kf_mmput) kf_mmput(mm);
 
         next_chunk:
             remaining -= chunk;
